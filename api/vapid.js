be5a35b10975856d2fc6ad3e86d7c7ef69fba7cd
matias20090714-sys <matias20090714@gmail.com>
2026-08-24 20:32:35 -0300

    Add api/vapid.js

---
+++ api/vapid.js
@@ -0,0 +1,6 @@
+const vapidKeys = {
+  publicKey: "BEl62iUYgUivxIkv69yViEuiBIa-Ib9-SkvMeAtA3LFgDzkrxZJjSgSnfckjBJuBkr3qBUYIHBQFLXYp5Nksh8U",
+  privateKey: "AAe_A8x9n-xL8zQO5RzPnLt0Fh0DvZWj8LuE7v8mQ9s"
+};
+
+module.exports = vapidKeys;
